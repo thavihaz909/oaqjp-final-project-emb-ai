26b64dbaa1c3bb0abef3b2a148aeafdbd1e00a35
Thavidu Gopaluwewa <thavihaz909@gmail.com>
2026-02-07 13:41:22 -0500

    Completed task 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 def emotion_detector(text_to_analyze):
@@ -5,4 +6,21 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"} 
     response = requests.post(url, json = myobj, headers=header)  
-    return response.text  
+
+    formatted_response = json.loads(response.text)
+
+    scores = formatted_response['emotionPredictions'][0]['emotion']
+    anger_score = scores['anger']
+    disgust_score = scores['disgust']
+    fear_score = scores['fear']
+    joy_score = scores['joy']
+    sadness_score = scores['sadness']
+
+    dominant_emotion = ''
+    highest_score = 0
+    for index, (key, value) in enumerate(scores.items()):
+        if value > highest_score:
+            highest_score = value
+            dominant_emotion = key
+
+    return {'anger': anger_score, 'disgust': disgust_score, 'fear': fear_score, 'joy': joy_score, 'sadness': sadness_score, 'dominant_emotion': dominant_emotion}    
